77b1534f3c13126ad3eb8fc9333d171ee824776a
Nathan Golba <ngolba@gmail.com>
2018-11-06 02:43:25 -0500

    initial build

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const mysql = require('mysql');
 require("dotenv").config();
 const serverPassword = process.env.serverPassword;
+let connection;
 
-const connection = mysql.createConnection({
+if (process.env.JAWSDB_URL){
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: serverPassword,
     database: 'burgers_db'
 })
+}
 
 connection.connect(err => {
     err ? console.error(`error connecting: ${err.stack}`) : console.log(`connected as id ${connection.threadId}`)
